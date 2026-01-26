b58a71735758fba6163a3de2efcc2929869f4ed4
Muttanna-halake <muttannahalakes@gmail.com>
2026-01-26 20:20:29 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -49,7 +49,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1kTAWAgJ-6VmNllWmIPQ-PuBW3GyFPQKd/view?usp=sharing"
+            href="https://drive.google.com/file/d/14r2RMy9kxiPOudmeK7tYJJLwnIMr6sk9/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
